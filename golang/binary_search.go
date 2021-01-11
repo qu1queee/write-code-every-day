e2a2c8472a624eb71b8f7fc981fd9823436c1a0d
qu1queee <encalada@de.ibm.com>
2021-01-11 21:42:40 +0100

    more funcs into basic binary search

--- golang/binary_search.go
+++ golang/binary_search.go
@@ -4,6 +4,8 @@ import "fmt"
 
 func main() {
 	fmt.Println(binarySearch([]int{1, 2, 4, 5, 9, 23, 12, 45}, 45))
+	fmt.Println(binarySearchLeftBorder([]int{2, 3, 5, 7}, 3))
+
 }
 
 func binarySearch(nums []int, target int) int {
@@ -20,5 +22,35 @@ func binarySearch(nums []int, target int) int {
 			right = mid - 1
 		}
 	}
+	return -1
+}
+
+func binarySearchLeftBorder(nums []int, target int) int {
+	var left int = 0
+	var right int = len(nums) - 1
+
+	// search interval is [left, right]
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] < target {
+			// search interval is [mid+1,right]
+			left = mid + 1
+		} else if nums[mid] > target {
+			// search interval is [left, mid-1]
+			left = mid - 1
+		} else if nums[mid] == target {
+			// shrink right border
+			right = mid - 1
+		}
+	}
+	// check out of bounds
+	if left >= len(nums) || nums[left] != target {
+		return -1
+	}
+
+	return left
+}
+
+func binarySearchRightBorder(nums []int, target int) int {
 	return -1
 }
