64edfcbdc2f224672a4d9786cf9e9f29f65dfb48
qu1queee <encalada@de.ibm.com>
2021-11-09 22:20:36 +0100

    Binary Tree Preorder traversal
    Added preorderTraversal func
    Added test cases

---
+++ golang/src/binary_tree/preorder_traversal/preorder_traversal_test.go
@@ -0,0 +1,45 @@
+package preorder_traversal
+
+import (
+	"reflect"
+	"testing"
+)
+
+func createBinaryTree(nums []int) *TreeNode {
+	return createTree(nums, 0)
+}
+
+func createTree(nums []int, index int) *TreeNode {
+	if index >= len(nums) {
+		return nil
+	}
+
+	tree := TreeNode{Val: nums[index]}
+	tree.Left = createTree(nums, 2*index+1)
+	tree.Right = createTree(nums, 2*index+2)
+	return &tree
+}
+
+func TestPreorderTraversal(t *testing.T) {
+	testCases := []*TreeNode{
+		createBinaryTree([]int{1, 2, 3, 4, 5, 6, 7, 8}),
+		createBinaryTree([]int{1, 2, 3}),
+		createBinaryTree([]int{1, 2, 5, 6}),
+	}
+	expected := [][]int{
+		{1, 2, 4, 8, 5, 3, 6, 7},
+		{1, 2, 3},
+		{1, 2, 6, 5},
+	}
+	testFuncs := []func(node *TreeNode) []int{
+		preorderTraversal,
+	}
+
+	for _, testFunc := range testFuncs {
+		for index, root := range testCases {
+			if res := testFunc(root); !reflect.DeepEqual(res, expected[index]) {
+				t.Errorf("got %v, expected %v", res, expected[index])
+			}
+		}
+	}
+}
