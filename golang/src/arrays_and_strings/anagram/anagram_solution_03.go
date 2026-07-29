78d7ea3c649a6d448c1e426d5643038f8e2f0d27
Enrique Encalada <encaladaenrique@gmail.com>
2026-07-29 21:04:42 +0200

    anagram 3rd

---
+++ golang/src/arrays_and_strings/anagram/anagram_solution_03.go
@@ -0,0 +1,21 @@
+package anagram
+
+func isAnagram(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+
+	var counts [26]int
+	for i := 0; i < len(s); i++ {
+		counts[s[i]-'a']++
+		counts[t[i]-'a']++
+
+	}
+	for _, v := range counts {
+		if v != 0 {
+			return false
+		}
+	}
+
+	return true
+}
